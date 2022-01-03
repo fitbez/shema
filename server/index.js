bdd360b046e7bc4c89f04079bd3d55e9815b77be
Fitsum <fitbez@gmail.com>
2022-01-03 12:11:17 -0500

    Product backend CRUD operation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 /** Setting up express server */
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.json()); // body parser
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running at port number 5000`);
 });
